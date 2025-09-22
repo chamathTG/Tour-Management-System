7fd6c95fc6d4a5e1342b7321cc7ee4b4d0b0712b
Chamath Dasun <chamathdasuntg@gmail.com>
2025-09-22 13:32:27 +0530

    Adding the code base back

---
+++ Tour Management System/windows/Login.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Tour_Management_System.windows
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BttnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
