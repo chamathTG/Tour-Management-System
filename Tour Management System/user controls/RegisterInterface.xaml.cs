44d8c550e6c8ffc14b4494c85d1a369d30fef7c6
Chamath Dasun <chamathdasuntg@gmail.com>
2025-09-23 20:49:09 +0530

    User control - RegisterInterface

---
+++ Tour Management System/user controls/RegisterInterface.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+using Tour_Management_System.windows;
+
+namespace Tour_Management_System.user_controls
+{
+    public partial class RegisterInterface : UserControl
+    {
+        public RegisterInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
