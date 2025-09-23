257287f4eca24cfe8ab078ab72d3cad96eaf7b8a
Chamath Dasun <chamathdasuntg@gmail.com>
2025-09-23 19:21:05 +0530

    User control - LoginInterface

---
+++ Tour Management System/user controls/LoginInterface.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Tour_Management_System.user_controls
+{
+    public partial class LoginInterface : UserControl
+    {
+        public LoginInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnRegister_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
