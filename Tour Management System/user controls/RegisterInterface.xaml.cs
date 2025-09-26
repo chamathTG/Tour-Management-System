a7b43a0d03bb6eefdedada7922ba59aa7d5c76c0
Chamath Dasun <chamathdasuntg@gmail.com>
2025-09-26 19:49:04 +0530

    Adding data input validations

--- Tour Management System/user controls/RegisterInterface.xaml.cs
+++ Tour Management System/user controls/RegisterInterface.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Tour_Management_System.windows;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Tour_Management_System.user_controls
 {
@@ -31,12 +35,148 @@ namespace Tour_Management_System.user_controls
 
         private void PbPassword_GotFocus(object sender, System.Windows.RoutedEventArgs e)
         {
-            GridPassRules.Visibility = System.Windows.Visibility.Visible;
+            GridPassRules.Visibility = Visibility.Visible;
         }
 
         private void PbPassword_LostFocus(object sender, System.Windows.RoutedEventArgs e)
         {
-            GridPassRules.Visibility = System.Windows.Visibility.Collapsed;
+            GridPassRules.Visibility = Visibility.Collapsed;
+        }
+
+        //Name
+        private void TbName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[a-zA-Z ]+$");
+        }
+
+        private void TbName_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if (!Regex.IsMatch(input, @"^[a-zA-Z ]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        //Username
+        private void TbUsername_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[a-zA-Z0-9]+$");
+        }
+
+        private void TbUsername_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if(!Regex.IsMatch(input, @"^[a-zA-Z0-9]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void TbUsername_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Email
+        private void TbEmail_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[a-zA-Z0-9@.]+$");
+        }
+
+        private void TbEmail_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if (!Regex.IsMatch(input, @"^[a-zA-Z0-9@.]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void TbEmail_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Contact Number
+        private void TbContactNumber_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[0-9-+]+$");
+        }
+
+        private void TbContactNumber_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if (!Regex.IsMatch(input, @"[0-9-+]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void TbContactNumber_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Password
+        private void PbPassword_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[a-zA-Z0-9.*$#@!&]+$");
+        }
+
+        private void PbPassword_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if (!Regex.IsMatch(input, @"^[a-zA-Z0-9.*$#@!&]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void PbPassword_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Reset Pin
+        private void PbResetPin_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"[a-zA-Z0-9.*$#@!&]+$");
+        }
+
+        private void PbResetPin_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string input = e.DataObject.GetData(DataFormats.Text) as string;
+
+            if (!Regex.IsMatch(input, @"^[a-zA-Z0-9.*$#@!&]+$"))
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void PbResetPin_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
