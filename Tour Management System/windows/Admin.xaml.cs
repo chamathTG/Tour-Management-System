5c18af3113668133a8fe796213f9eca9bab1151b
Chamath Dasun <chamathdasuntg@gmail.com>
2025-09-28 02:14:56 +0530

    Setting up the window control buttons in Admin window

--- Tour Management System/windows/Admin.xaml.cs
+++ Tour Management System/windows/Admin.xaml.cs
@@ -8,5 +8,23 @@ namespace Tour_Management_System.windows
         {
             InitializeComponent();
         }
+
+        private void BttnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Login windowLogin = new Login();
+            windowLogin.Show();
+
+            this.Close();
+        }
+
+        private void BttnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BttnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
